ccc1f535a86bd8291a0a54f34fc13b86cd145419
ztinyz <bardirobert1@gmail.com>
2024-05-08 14:36:02 +0300

    start

---
+++ project_1/project_1.srcs/sources_1/new/MAIN.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/08/2024 02:28:49 PM
+-- Design Name: 
+-- Module Name: MAIN - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MAIN is
+    Port ( BTNL,BTNR,BTNU,BTND,CLK : in STD_LOGIC;
+           VGA : out STD_LOGIC);
+end MAIN;
+
+architecture Behavioral of MAIN is
+
+begin
+
+
+end Behavioral;
